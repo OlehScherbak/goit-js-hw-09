ed5428214b6c1d71c56e918e3404a44b86055e54
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-11-26 12:20:43 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+let timerId;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  timerId = setInterval(bgcololChange, 1000);
+  btnStart.disabled = true;
+}
+
+function bgcololChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopClick() {
+  clearTimeout(timerId);
+  btnStart.disabled = false;
+}
