1cbd767281fcb5b1a241a0793d35b80998425142
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-11-27 16:49:09 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay,
+        });
+      } else {
+        reject({
+          position,
+          delay,
+        });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', startPromiseChain);
+
+function startPromiseChain(evt) {
+  evt.preventDefault();
+  const initDelay = Number(document.querySelector('[name="delay"]').value);
+  const step = Number(document.querySelector('[name="step"]').value);
+  const amount = Number(document.querySelector('[name="amount"]').value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    const delay = initDelay + step * (position - 1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
